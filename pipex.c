add21568c3eedf98d615d808121f5caef7249ee4
love <tlove.tky@gmail.com>
2023-02-11 17:15:06 +0900

    pipe 1本のみ完了

--- pipex.c
+++ pipex.c
@@ -6,27 +6,74 @@
 /*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 10:57:28 by terabu            #+#    #+#             */
-/*   Updated: 2023/02/11 16:06:00 by terabu           ###   ########.fr       */
+/*   Updated: 2023/02/11 17:04:45 by terabu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char *argv[])
+void	do_child(int i_fd[], char *outfile, char **cmd)
 {
-	char	**cmd_line;
-	int		i;
-	int		fd[2];
+	int	fd;
 
-	(void)argc;
-	cmd_line = get_cmd_array(argv[2]);
-	i = 0;
+	close(i_fd[1]);
+	// input pipe
+	close(STDIN_FILENO);
+	dup2(i_fd[0], STDIN_FILENO);
 
-	fd[0] = open(argv[1], O_RDONLY);
-	close(0);
-	dup2(fd[0], 0);
-	close(fd[0]);
-	execve(cmd_line[0], cmd_line, environ);
-	return (0);
+	// output file
+	fd = open(outfile, O_WRONLY);
+	dup2(fd, STDOUT_FILENO);
+	execve(cmd[0], cmd, environ);
+	close(i_fd[0]);
 }
 
+void	do_parent(int o_fd[], char *infile, char **cmd)
+{
+	int	fd;
+	// int status;
+
+	close(o_fd[0]);
+	// input infile
+	fd = open(infile, O_RDONLY);
+	close(STDIN_FILENO);
+	dup2(fd, STDIN_FILENO);
+
+	// output pipe
+	close(STDOUT_FILENO);
+	dup2(o_fd[1], STDOUT_FILENO);
+	// if (wait(&status) < 0)
+	// {
+	// 	perror("wait");
+	// 	exit(1);
+	// }
+	execve(cmd[0], cmd, environ);
+	close(fd);
+}
+
+int	main(int argc, char *argv[])
+{
+	char	**cmd_line1;
+	char	**cmd_line2;
+	int		fd[2];
+	pid_t	pid;
+
+	(void)argc;
+	cmd_line1 = get_cmd_array(argv[2]);
+	cmd_line2 = get_cmd_array(argv[3]);
+	pipe(fd);
+	pid = fork();
+
+	if (pid)
+		do_parent(fd, argv[1], cmd_line1);
+	else
+	{
+		do_child(fd, argv[4], cmd_line2);
+	}
+	// fd[0] = open(argv[1], O_RDONLY);
+	// close(0);
+	// dup2(fd[0], 0);
+	// close(fd[0]);
+	// execve(cmd_line[0], cmd_line, environ);
+	return (0);
+}
