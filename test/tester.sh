608da40cbc6f4193a0a7769e5918e5477992b513
love <tlove.tky@gmail.com>
2023-02-14 12:38:23 +0900

    add:testcase

--- test/tester.sh
+++ test/tester.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## 通常ケース
+#---- 通常ケース ----#
 # 本家
 <infile grep a1 | wc> outfile_sh
 
@@ -12,13 +12,24 @@
 diff -U 3 outfile_sh outfile && echo "OK" || echo "KO"
 
 
-## infile NULLケース
+#----2 infile NULLケース ----#
 # 本家
-<infile_n grep a1 | wc> outfile_sh
+<infile_2_n grep a1 | wc> outfile_sh
 
 # 自作
-../pipex infile_n "grep a1" "wc" outfile
+../pipex infile_2_n "grep a1" "wc" outfile
 
 # outを比較
 /bin/echo -n '2:'
+diff -U 3 outfile_sh outfile && echo "OK" || echo "KO"
+
+#----3 infile 未使用ケース ----#
+# 本家
+< infile_3_nouse ls -l | wc -l > outfile_sh
+
+# 自作
+../pipex infile_3_nouse "ls -l" "wc -l" outfile
+
+# outを比較
+/bin/echo -n '3:'
 diff -U 3 outfile_sh outfile && echo "OK" || echo "KO"
